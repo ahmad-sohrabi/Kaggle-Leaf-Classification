5072e48a33b31e8a4ed0d98e50514ef28129772e
Ahmad Sohrabi <ahmadsohrabi69@gmail.com>
2024-01-04 23:42:03 +0330

    very little modification on some comments

--- CustomFunctions.py
+++ CustomFunctions.py
@@ -9,7 +9,7 @@ import numpy as np
 
 
 def accuracy_fn(y_true, y_pred):
-    correct = torch.eq(y_true, y_pred).sum().item()  # torch.eq() calculates where two tensors are equal
+    correct = torch.eq(y_true, y_pred).sum().item()
     acc = (correct / len(y_pred)) * 100
     return acc
 
